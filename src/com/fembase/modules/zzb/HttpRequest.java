f9bafee61e4ffd6c79d6e8a51413b8315dc150d3
irady-mrbean <1541329987@qq.com>
2015-11-21 00:59:31 +0800

    setting

--- src/com/fembase/modules/zzb/HttpRequest.java
+++ src/com/fembase/modules/zzb/HttpRequest.java
@@ -52,7 +52,7 @@ public class HttpRequest {
             }
             // 定义 BufferedReader输入流来读取URL的响应
             in = new BufferedReader(new InputStreamReader(
-                    connection.getInputStream()));
+                    connection.getInputStream(),"UTF-8"));
             String line;
             while ((line = in.readLine()) != null) {
                 result += line;
@@ -133,7 +133,7 @@ public class HttpRequest {
         return result;
     }    
     
-    public static void main(String arg[]){
+    public  void requestIpAction(){
     	
     	DealIpEmtityService service=new DealIpEmtityServiceImpl();
     	
